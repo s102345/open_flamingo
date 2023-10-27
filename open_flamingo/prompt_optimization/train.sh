f976cb7a8ec87e79680f88dd5d10f415118e30cc
s102345 <Josephchen102345@gmail.com>
2023-10-27 23:07:14 +0800

    Test on 國網中心

--- open_flamingo/prompt_optimization/train.sh
+++ open_flamingo/prompt_optimization/train.sh
@@ -2,7 +2,6 @@ python ./open_flamingo/prompt_optimization/manager.py \
 --output_dir ./ \
 --steps 5 \
 --rices \
---cross_attn_every_n_layers 2 \
 --shots 4 \
 --example_rule "rand" \
 --caption_number 3 \
