5d24a6f1c047364ed01afe461b92fe1eeea78d4e
s102345 <Josephchen102345@gmail.com>
2023-10-30 00:15:47 +0800

    Update ipynb for use

--- open_flamingo/prompt_optimization/train.sh
+++ open_flamingo/prompt_optimization/train.sh
@@ -1,10 +1,9 @@
-python ./open_flamingo/prompt_optimization/manager.py \
+python ./manager.py \
 --output_dir ./ \
 --steps 1 \
 --rices \
 --shots 4 \
 --example_rule "rices" \
---caption_number 3 \
 --initial_prompt "Output: " \
 --extra_information \
 
